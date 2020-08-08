7893171b92466ddd2139918b9265a746b58280f6
Josh Ellis <joshe523@gmail.com>
2020-08-08 10:38:17 -0500

    Revert to pre carbon

--- src/app/components/MiniCard/MiniCardView.tsx
+++ src/app/components/MiniCard/MiniCardView.tsx
@@ -3,7 +3,7 @@ import Task from '../Task'
 import { MiniCardPropTypes } from './MiniCardTypes'
 
 export const MiniCardView: FunctionComponent<MiniCardPropTypes> = ({ kind }) => (
-  <div className='px-4 py-2 bg-gray-90 rounded grid grid-cols-4 items-center gap-1 text-sm font-bold'>
+  <div className='px-4 py-2 bg-gray-800 rounded grid grid-cols-4 items-center gap-1 text-sm font-bold'>
     <div>{kind}</div>
     <Task name='n' dotOnly={true} />
     <Task name='n' dotOnly={true} />
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import App from './app'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
+import './tailwind.output.css'
 
 ReactDOM.render(
   <React.StrictMode>
