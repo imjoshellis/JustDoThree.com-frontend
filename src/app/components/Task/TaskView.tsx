194754f79dc8217a1aa08af315922b23ec942136
Josh Ellis <joshe523@gmail.com>
2020-08-08 13:32:06 -0500

    Add disabled prop

--- src/app/components/Task/TaskView.tsx
+++ src/app/components/Task/TaskView.tsx
@@ -7,13 +7,22 @@ export const TaskView: FunctionComponent<TaskPropTypes> = ({
   status,
   handleClick,
   hover,
-  dotOnly
+  dotOnly,
+  disabled
 }) => {
   let classes = {
-    base: 'flex flex-row items-center p-1 w-full rounded',
-    hover: dotOnly ? '' : 'font-bold bg-gray-700'
+    base: 'flex flex-row items-center p-1 w-full rounded focus:bg-gray-80',
+    hover: dotOnly ? '' : 'font-bold bg-gray-70'
   }
 
+  if (disabled) return (
+    <div
+      className={classes.base}
+    >
+      <Dot status={status} hover={false} />
+      { dotOnly || <><span className='w-2' /> {name}</> }
+    </div>
+  )
   return (
     <button
       onClick={handleClick}
