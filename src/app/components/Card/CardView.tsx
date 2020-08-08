30fdefb2094b4465d59498a5210e8b0aea2493d6
Josh Ellis <joshe523@gmail.com>
2020-08-07 20:06:24 -0500

    Update css

--- src/app/components/Card/CardView.tsx
+++ src/app/components/Card/CardView.tsx
@@ -2,19 +2,28 @@ import React, { FunctionComponent } from 'react'
 import Task from '../Task'
 import { CardPropTypes } from './CardTypes'
 
-export const CardView: FunctionComponent<CardPropTypes> = ({ kind }) => (
-  <div className='bg-gray-800 rounded overflow-hidden'>
-    <div className='h-32 overflow-hidden'>
-      <img src='https://images.unsplash.com/photo-1596806970810-8734072b5d4d' alt='alt' />
-    </div>
+export const CardView: FunctionComponent<CardPropTypes> = ({ kind }) => {
+  let classes = {
+    base: 'bg-gray-800 rounded-lg rounded-b overflow-hidden',
+    hover: ''
+  }
+  return (
+    <div className={classes.base}>
+      <div className='h-32 overflow-hidden'>
+        <img
+          src='https://images.unsplash.com/photo-1596806970810-8734072b5d4d'
+          alt='alt'
+        />
+      </div>
 
-    <div className='p-4'>
-      <h2 className='uppercase text-sm font-bold tracking-wider mb-2'>{kind}</h2>
-      <Task name='Task' status='done' />
-      <Task name='Task' />
-      <Task name='Task' />
+      <div className='p-4'>
+        <h2 className='uppercase text-sm font-bold tracking-wider mb-2'>{kind}</h2>
+        <Task name='Task' status='done' />
+        <Task name='Task' />
+        <Task name='Task' />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default CardView
