274d2b263ef82ccecb55ab21baf94f19f9edb432
Josh Ellis <joshe523@gmail.com>
2020-08-07 20:15:21 -0500

    Add test for main and footer

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -11,3 +11,13 @@ it('renders a <Header />', () => {
   const wrapper = shallow(<App />)
   expect(wrapper.contains(<Header />)).toEqual(true)
 })
+
+it('renders a <Main />', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.contains(<Main />)).toEqual(true)
+})
+
+it('renders a <Footer />', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper.contains(<Footer />)).toEqual(true)
+})
