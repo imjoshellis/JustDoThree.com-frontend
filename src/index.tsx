b4d33b0b647d334275ff4246ee1f61dcf11ae6cf
Josh Ellis <joshe523@gmail.com>
2020-08-10 11:55:47 -0500

    Add basic task reducers

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,37 @@ import './index.css'
 import * as serviceWorker from './serviceWorker'
 import './tailwind.output.css'
 
+const ADD_TASK = 'ADD_TASK'
+const REMOVE_TASK = 'REMOVE_TASK'
+const TOGGLE_TASK = 'TOGGLE_TASK'
+
+interface Action {
+  type: string
+  payload?: any
+  id?: number
+}
+
+const task = ({ state = [], action }: { state: Array<object>; action: Action }) => {
+  interface Task {
+    id: number
+  }
+  switch (action.type) {
+    case ADD_TASK:
+      return [...state, action.payload]
+    case REMOVE_TASK:
+      return state.filter((task: { id?: number }): boolean => task.id !== action.id)
+    case TOGGLE_TASK:
+      return state.map((task: { id?: number }): any => {
+        if (task.id === action.id) {
+          return { ...task, completed: true }
+        }
+        return task
+      })
+    default:
+      return state
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
