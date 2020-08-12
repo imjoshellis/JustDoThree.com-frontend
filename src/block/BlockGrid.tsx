738320be62810f5d1e30f4df5e2c9680c257624b
Josh Ellis <joshe523@gmail.com>
2020-08-12 14:28:10 -0500

    Adjust Formatting

--- src/block/BlockGrid.tsx
+++ src/block/BlockGrid.tsx
@@ -10,13 +10,11 @@ interface Props {
 export const BlockGridView: FunctionComponent<Props> = ({
   topBlocks,
   bottomBlocks
-}) => {
-  return (
-    <div className='grid grid-rows-2 gap-4'>
-      <TaskBlockRow blocks={topBlocks} />
-      <TaskBlockRow blocks={bottomBlocks} />
-    </div>
-  )
-}
+}) => (
+  <div className='grid grid-rows-2 gap-4'>
+    <TaskBlockRow blocks={topBlocks} />
+    <TaskBlockRow blocks={bottomBlocks} />
+  </div>
+)
 
 export default BlockGridView
