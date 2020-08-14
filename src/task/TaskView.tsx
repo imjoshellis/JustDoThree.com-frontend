e9f9202376e4bf1c6e6b276c6f0414227ba79972
Josh Ellis <joshe523@gmail.com>
2020-08-14 13:34:07 -0500

    Move classes inline

--- src/task/TaskView.tsx
+++ src/task/TaskView.tsx
@@ -6,10 +6,8 @@ interface TaskPropTypes {
   title: string
   completed: boolean
   dotOnly?: boolean
-  hover: boolean
   disabled?: boolean
   toggleTask: ActionCreatorWithPayload<any>
-  toggleEdit: () => void
   editing: boolean
 }
 
@@ -17,56 +15,32 @@ export const TaskView: FunctionComponent<TaskPropTypes> = ({
   id,
   title,
   completed,
-  hover,
   toggleTask,
   dotOnly,
-  toggleEdit,
   disabled
 }) => {
-  let checkBase =
-    'check-input rounded border-solid border-2 mt-1 appearance-none p-15 w-1 h-1 relative cursor-pointer hover:border-gray-60'
-  if (disabled) {
-    checkBase += ' pointer-events-none'
-  }
-  let classes = {
-    base:
-      'p-1 px-2 w-full rounded flex select-none cursor-pointer transition-all duration-200 ease-out hover:bg-gray-85',
-    checkbox: {
-      done: checkBase + ' border-gray-70 bg-gray-70 text-gray-90',
-      todo: checkBase + ' border-gray-80 bg-gray-100 text-gray-10'
-    },
-    label: {
-      base: 'ml-2 text-left text-sm transition-all duration-200 cursor-pointer',
-      done: 'opacity-50'
-    }
-  }
-
-  const checkboxClass = completed
-    ? classes.checkbox.done
-    : classes.checkbox.todo
-
-  if (!dotOnly) {
-    classes.base += ' focus:bg-gray-80 mt-1'
-  }
-
   return (
-    <div className='flex items-center justify-between'>
-      <form className={classes.base} onClick={() => toggleTask(id)}>
+    <div
+      className={`flex items-center justify-between w-full p-1 px-2 transition-all duration-200 ease-out rounded cursor-pointer select-none hover:bg-gray-85 ${disabled &&
+        'pointer-events-none'} ${dotOnly || 'focus:bg-gray-80 mt-1'}`}
+    >
+      <form className='flex' onClick={() => toggleTask(id)}>
         <input
           type='checkbox'
           disabled={disabled}
           name={'task-' + id}
-          className={checkboxClass}
+          className={`check-input rounded border-solid border-2 mt-1 appearance-none p-15 w-1 h-1 relative cursor-pointer hover:border-gray-60 ${
+            completed
+              ? 'border-gray-70 bg-gray-70 text-gray-90'
+              : 'border-gray-80 bg-gray-100 text-gray-10'
+          }`}
           checked={completed}
         />
         {dotOnly || (
           <>
             <label
-              className={
-                completed
-                  ? classes.label.base + ' ' + classes.label.done
-                  : classes.label.base
-              }
+              className={`ml-2 text-left text-sm transition-all duration-200 cursor-pointer ${completed &&
+                'opacity-50'}`}
             >
               {title}
             </label>
