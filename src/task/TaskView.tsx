d24fa70e3e428817af2b8b5d73c406f88b247550
Josh Ellis <josh@imjoshellis.com>
2020-08-19 15:44:48 -0500

    Add transition to checkbox

--- src/task/TaskView.tsx
+++ src/task/TaskView.tsx
@@ -37,7 +37,7 @@ export const TaskView: FunctionComponent<Props> = ({
           type='checkbox'
           disabled={disabled || isDragging}
           name={'task-' + id}
-          className={`check-input rounded border-solid border-2 mt-1 appearance-none p-15 w-1 h-1 relative cursor-pointer hover:border-gray-60 ${
+          className={`check-input rounded border-solid border-2 mt-1 appearance-none p-15 w-1 h-1 relative focus:outline-none transition duration-200 cursor-pointer hover:border-gray-60 ${
             completed
               ? 'border-gray-50 bg-gray-50 text-gray-90'
               : 'border-gray-80 bg-gray-100 text-gray-10'
