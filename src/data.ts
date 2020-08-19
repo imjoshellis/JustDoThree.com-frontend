ddda7d3378cbd0d6d8d7063d61ad6a50eb15ab26
Josh Ellis <josh@imjoshellis.com>
2020-08-19 11:45:04 -0500

    Remove dates

--- src/data.ts
+++ src/data.ts
@@ -25,31 +25,40 @@ export const data = {
     tasks: {
       1: {
         completed: false,
-        dueDate: moment().add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd'),
+        // dueDate: moment()
+        // .add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd')
+        // .toDate(),
         id: 1,
         title: 'I became a Senior Developer at a high growth organization'
       },
       2: {
         completed: true,
-        dueDate: moment().add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd'),
+        // dueDate: moment()
+        // .add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd')
+        // .toDate(),
         id: 2,
         title: 'I published a board game'
       },
       3: {
         completed: false,
-        dueDate: moment().add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd'),
+        // dueDate: moment()
+        // .add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd')
+        // .toDate(),
         id: 3,
         title: 'I ran a marathon'
       },
       4: {
         completed: false,
-        dueDate: moment().add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd'),
+        // dueDate: moment()
+        // .add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd')
         id: 4,
         title: 'I did one thing this year'
       },
       5: {
         completed: true,
-        dueDate: moment().add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd'),
+        // dueDate: moment()
+        // .add(Math.random() * 1000 * (Math.random() < 0.5 ? -1 : 1), 'd')
+        // .toISOString(),
         id: 5,
         title: 'I did another thing this year'
       }
