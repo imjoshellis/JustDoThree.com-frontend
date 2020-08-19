e2c7cd5164cdfcf9eeb0ba6751c7830bc4a13b3b
Josh Ellis <josh@imjoshellis.com>
2020-08-19 11:46:04 -0500

    Remove unnecessary nesting in BlockRow

--- src/block/BlockRow.tsx
+++ src/block/BlockRow.tsx
@@ -13,15 +13,7 @@ export const BlockRow: FunctionComponent<Props> = ({
 }) => (
   <div className='flex flex-col mt-4 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-4'>
     {blocks.map((b: BlockTypes) => (
-      <>
-        {b && (
-          <ConnectedBlock
-            block={b}
-            key={b.id}
-            changeTopBlock={changeTopBlock}
-          />
-        )}
-      </>
+      <ConnectedBlock block={b} key={b.id} changeTopBlock={changeTopBlock} />
     ))}
   </div>
 )
