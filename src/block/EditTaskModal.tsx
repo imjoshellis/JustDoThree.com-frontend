79f32f21e58c493b86b8b2eb6c38bf3a18f09721
Josh Ellis <josh@imjoshellis.com>
2020-08-20 18:19:41 -0500

    Change animation duration

--- src/block/EditTaskModal.tsx
+++ src/block/EditTaskModal.tsx
@@ -32,7 +32,7 @@ export const EditTaskModal: React.FC<EditTaskModalProps> = ({
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{
-                duration: 0.3,
+                duration: 0.2,
                 ease: 'circOut'
               }}
               className='absolute top-0 bottom-0 left-0 right-0 z-50 flex items-center justify-center bg-gray-100 bg-opacity-75'
