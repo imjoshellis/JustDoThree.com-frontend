34d26ffbb2ad27fe314ec2ed4db8551bc8ceca2b
Josh Ellis <josh@imjoshellis.com>
2020-08-21 09:37:55 -0500

    Setup Increment / Decrement buttons

--- src/block/BlockRow.tsx
+++ src/block/BlockRow.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ConnectedBlock from './Block'
 import { BlockTypes } from './blocksSlice'
+import { AnimatePresence } from 'framer-motion'
 
 interface Props {
   blocks: BlockTypes[]
@@ -14,18 +15,47 @@ export const BlockRow: React.FC<Props> = ({
   setTopBlock,
   sourceBlock,
   destinationBlock
-}) => (
-  <div className='flex flex-col mt-4 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-4'>
-    {blocks.map((b: BlockTypes) => (
-      <ConnectedBlock
-        block={b}
-        key={b.id}
-        sourceBlock={sourceBlock}
-        destinationBlock={destinationBlock}
-        setTopBlock={setTopBlock}
-      />
-    ))}
-  </div>
-)
+}) => {
+  const [idx, setIdx] = useState(0)
+
+  const Btn = ({ text, onClick }: { text: string; onClick: () => void }) => (
+    <button
+      className='p-2 py-1 m-1 text-sm font-bold tracking-wider rounded bg-blue-50'
+      onClick={() => onClick()}
+    >
+      {text}
+    </button>
+  )
+
+  const IncIdxBtn = () => (
+    <Btn onClick={() => setIdx(idx + 1)} text='increment' />
+  )
+
+  const DecIdxBtn = () => (
+    <Btn onClick={() => setIdx(idx - 1)} text='decrement' />
+  )
+
+  return (
+    <div className='md:max-w-2xl'>
+      <div className='flex flex-col mt-4 md:grid md:grid-cols-2 md:content-between md:gap-4 lg:grid-cols-4'>
+        {blocks.slice(idx, idx + 4).map((b: BlockTypes) => (
+          <ConnectedBlock
+            block={b}
+            key={b.id}
+            sourceBlock={sourceBlock}
+            destinationBlock={destinationBlock}
+            setTopBlock={setTopBlock}
+          />
+        ))}
+      </div>
+      {blocks.length > 4 && (
+        <div className='flex justify-between mt-1'>
+          <DecIdxBtn />
+          <IncIdxBtn />
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default BlockRow
