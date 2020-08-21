e981ba045a00730ae2696f69b21edf4f08808abb
Josh Ellis <josh@imjoshellis.com>
2020-08-21 11:43:30 -0500

    Add interactive styles to block title

--- src/block/Block.tsx
+++ src/block/Block.tsx
@@ -47,7 +47,7 @@ export const Block: React.FC<Props> = ({
             }`}
           >
             <h2
-              className='px-2 mb-2 text-sm font-bold tracking-wider uppercase'
+              className='px-2 mb-2 text-sm font-bold tracking-wider uppercase cursor-pointer hover:underline hover:text-blue-50'
               onClick={() => setTopBlock()}
             >
               {block.title}
