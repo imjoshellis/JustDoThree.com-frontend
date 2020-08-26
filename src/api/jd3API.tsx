2c23709c80a9470ba16085fdf8a45c60ca14a9f6
Josh Ellis <josh@imjoshellis.com>
2020-08-26 13:44:02 -0500

    Add task API

--- src/api/jd3API.tsx
+++ src/api/jd3API.tsx
@@ -21,3 +21,26 @@ export async function getBlocks (): Promise<BlockAPI[]> {
   const { data } = await axios.get<BlockData>(url)
   return data.data
 }
+
+export interface TaskAPI {
+  id: string
+  type: string
+  attributes: {
+    title: string
+    due_date: string
+    block_id: string
+    block_title: string
+    completed: boolean
+  }
+}
+
+export interface TaskData {
+  data: TaskAPI[]
+}
+
+export async function getTasks (): Promise<TaskAPI[]> {
+  const url = `http://localhost:5000/tasks/`
+
+  const { data } = await axios.get<TaskData>(url)
+  return data.data
+}
