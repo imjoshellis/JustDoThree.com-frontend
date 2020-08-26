d299ce9fcb76e68d16b37520a2bb20a2f6bf9a64
Josh Ellis <josh@imjoshellis.com>
2020-08-26 13:44:59 -0500

    Add fetch tasks

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,19 +5,26 @@ import Footer from './Footer'
 import Main from './Main'
 import { fetchBlocks } from '../block/blocksSlice'
 import { RootState } from './reducer'
+import { fetchTasks } from '../task/tasksSlice'
 
 function App (): JSX.Element {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(fetchBlocks())
+    dispatch(fetchTasks())
   }, [dispatch])
 
   const blocks = useSelector((state: RootState) => state.blocks)
+  const tasks = useSelector((state: RootState) => state.tasks)
 
   return (
     <div className='absolute top-0 bottom-0 left-0 right-0 pt-8'>
       <Header />
-      {Object.keys(blocks).length > 0 ? <Main /> : 'Loading Data...'}
+      {Object.keys(blocks).length > 0 && Object.keys(tasks).length > 0 ? (
+        <Main />
+      ) : (
+        'Loading Data...'
+      )}
       <Footer />
     </div>
   )
