cb8ef7ef75263568f45b85024fe6f332080418a2
Josh Ellis <josh@imjoshellis.com>
2020-08-26 12:03:40 -0500

    Add async API call

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import Footer from './Footer'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import Header from '../header'
+import Footer from './Footer'
 import Main from './Main'
+import { fetchBlocks } from '../block/blocksSlice'
+import { RootState } from './reducer'
 
 function App (): JSX.Element {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(fetchBlocks())
+  }, [dispatch])
+
+  const blocks = useSelector((state: RootState) => state.blocks)
+
   return (
     <div className='absolute top-0 bottom-0 left-0 right-0 pt-8'>
       <Header />
-      <Main />
+      {Object.keys(blocks).length > 0 ? <Main /> : 'Loading Data...'}
       <Footer />
     </div>
   )
