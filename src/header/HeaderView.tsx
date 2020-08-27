5cbbb6d3b930faed9c565cf4c814bc0ace4bb330
Josh Ellis <josh@imjoshellis.com>
2020-08-26 20:49:50 -0500

    Add routing

--- src/header/HeaderView.tsx
+++ src/header/HeaderView.tsx
@@ -2,10 +2,31 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { TaskPropTypes } from '../task/TaskContainer'
 import { HeaderPropTypes } from './HeaderTypes'
+import { Link } from 'react-router-dom'
 
 export const HeaderView: React.FC<HeaderPropTypes> = ({ tasks }) => (
   <header className='fixed top-0 left-0 right-0 z-50 flex flex-row items-center justify-between p-4 px-16 bg-gray-900'>
-    <div>Just Do Three</div>
+    <div className='text-xl font-bold'>Just Do Three</div>
+    <div className='flex gap-3'>
+      <Link
+        className='flex items-center justify-center p-1 px-3 text-xs font-bold tracking-wider uppercase transition duration-200 rounded hover:bg-gray-70 bg-gray-90'
+        to='/'
+      >
+        soon
+      </Link>
+      <Link
+        className='flex items-center justify-center p-1 px-3 text-xs font-bold tracking-wider uppercase transition duration-200 rounded hover:bg-gray-70 bg-gray-90'
+        to='/today'
+      >
+        today
+      </Link>
+      <Link
+        className='flex items-center justify-center p-1 px-3 text-xs font-bold tracking-wider uppercase transition duration-200 rounded hover:bg-gray-70 bg-gray-90'
+        to='/all'
+      >
+        all
+      </Link>
+    </div>
     {/*
       <div className='grid flex-grow max-w-3xl grid-cols-6 gap-4'>
         <MiniCard kind='L' tasks={tasks} />
