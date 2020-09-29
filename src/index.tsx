807da61d22f256d6c74ddfc06997b1ecc19170f8
Josh Ellis <josh@imjoshellis.com>
2020-09-29 15:09:01 -0500

    Add firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,29 @@
+import * as firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/auth'
+import 'firebase/firestore'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './app'
+import store from './app/store'
 import './index.css'
 import './tailwind.output.css'
-import store from './app/store'
+
+const firebaseConfig = {
+  apiKey: process.env.firebaseKey,
+  authDomain: process.env.firebaseAuthDomain,
+  databaseURL: process.env.firebaseDatabaseURL,
+  projectId: process.env.firebaseProjectId,
+  storageBucket: process.env.firebaseStorageBucket,
+  messagingSenderId: process.env.firebaseSenderId,
+  appId: process.env.fierbaseAppId,
+  measurementId: process.env.firebaseMeasurementId
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,8 +33,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister()
